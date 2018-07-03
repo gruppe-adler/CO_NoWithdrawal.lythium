7f1d0c022f3cc00c9579f4ae957bc039719ac0eb
Eras308 <31324871+Eras308@users.noreply.github.com>
2018-07-03 19:45:27 +0200

    Fortifications

--- USER/userFortifications.hpp
+++ USER/userFortifications.hpp
@@ -10,5 +10,9 @@ fortificationOwnerType = "SIDE";
 demolitionTimeFactor = 1.4;
 
 class Fortifications {
-
+[Kiste_fort,"Land_BagFence_Long_F",2] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_BagFence_Short_F",5] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_BagFence_Corner_F",4] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_BagFence_Round_F",2] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_Razorwire_F",1] call grad_fortifications_fnc_addFort;
 };
