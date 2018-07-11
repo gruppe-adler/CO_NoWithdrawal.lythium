46057d7f909829d4d5b4971d484c78e61c3aae06
Eras308 <31324871+Eras308@users.noreply.github.com>
2018-07-11 21:37:51 +0200

    FIXES

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -5,6 +5,6 @@
 author = "Eras & Ainer für Gruppe Adler";                                               // Missionsersteller
 onLoadName = "No Withdrawal";                                                   // Name der Mission
 onLoadMission = "Do not risk our freedom";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
-loadScreen = "data\loadpic.paa";                                                // Ladebild
+loadScreen = "data\Isreal.jpg";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
 overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
