5bceb87bc2179b47f73f7186b686b4ac6ddd96a8
Eras308 <31324871+Eras308@users.noreply.github.com>
2018-07-15 18:30:40 +0200

    Fix flag, Fort, Medic loadout, Kisten,

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -5,6 +5,6 @@
 author = "Eras & Ainer für Gruppe Adler";                                               // Missionsersteller
 onLoadName = "No Withdrawal";                                                   // Name der Mission
 onLoadMission = "Do not risk our freedom";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
-loadScreen = "data\Isreal.jpg";                                                // Ladebild
+loadScreen = "data\loadpic.jpg";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
 overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
--- initServer.sqf
+++ initServer.sqf
@@ -4,4 +4,5 @@
 [Kiste_fort,"Land_BagFence_Short_F",5] call grad_fortifications_fnc_addFort;
 [Kiste_fort,"Land_BagFence_Corner_F",4] call grad_fortifications_fnc_addFort;
 [Kiste_fort,"Land_BagFence_Round_F",2] call grad_fortifications_fnc_addFort;
-[Kiste_fort,"Land_Razorwire_F",1] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_Razorwire_F",4] call grad_fortifications_fnc_addFort;
+[Kiste_fort,"Land_CzechHedgehog_01_new_F",4] call grad_fortifications_fnc_addFort;
